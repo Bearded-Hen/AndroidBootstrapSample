7d8a6f5a1ab1e9958af205ecaaa000ec5143c4f5
JAMIE LYNCH <jamie@beardedhen.com>
2015-02-09 18:08:11 +0000

    initial commit

---
+++ app/src/main/java/com/beardedhen/androidbootstrapsample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.beardedhen.androidbootstrapsample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+           switchDisplayedFragment(new PlaceholderFragment());
+        }
+    }
+
+    private void switchDisplayedFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
